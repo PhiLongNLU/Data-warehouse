8839bc775e081743e437ffe92dc290a6e4c5c4f2
Nguyen-Thi-Thanh-Ngan <21130116@st.hcmuaf.edu.vn>
2024-12-10 22:29:58 +0700

    Cập nhật các thay đổi trong Connector và ExtractDataToStaging

--- crawldata/src/main/java/org/example/Connector/DBProperties.java
+++ crawldata/src/main/java/org/example/Connector/DBProperties.java
@@ -6,11 +6,13 @@ import java.util.Properties;
 public class DBProperties {
     private static Properties propStaging = new Properties();
     private static Properties propDataWarehouse = new Properties();
+    private static Properties propControl = new Properties();
 
     static {
         try {
             propStaging.load(DBProperties.class.getClassLoader().getResourceAsStream("staging.properties"));
             propDataWarehouse.load(DBProperties.class.getClassLoader().getResourceAsStream("data_warehouse.properties"));
+            propControl.load(DBProperties.class.getClassLoader().getResourceAsStream("config.properties"));
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException();
@@ -30,4 +32,11 @@ public class DBProperties {
     public static String usernameData = propDataWarehouse.getProperty("db.username");
     public static String passData = propDataWarehouse.getProperty("db.password");
     public static String dbnameData = propDataWarehouse.getProperty("db.name");
+
+    // Sử dụng propControl cho control enviroment
+    public static String hostControl = propControl.getProperty("db.host");
+    public static String portControl = propControl.getProperty("db.port");
+    public static String usernameControl = propControl.getProperty("db.username");
+    public static String passControl = propControl.getProperty("db.password");
+    public static String dbnameControl = propControl.getProperty("db.name");
 }
